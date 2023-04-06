d89175600793ee72572dd3e9804884ab0b120936
unknown <thomasjjohnson9311@gmail.com>
2023-04-06 17:44:39 -0500

    partially set up function

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,71 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let lowerAlphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
+let upperAlphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let builtArray = [...upperAlphabet, ...numbers];
+let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
+
+// lowerAlphabetExample[generatedLetterIndex];
 function generatePassword() {
+  if (confirm("Do you want your password to contain lowercase letters?")) {
+  }
+
   return "password example";
 }
 
