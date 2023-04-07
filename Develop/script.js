6037f9f24ccaaf39d778e05954227fc09839de4a
unknown <thomasjjohnson9311@gmail.com>
2023-04-06 23:25:24 -0500

    commented code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// place all possible numbers in an array & assign to a variable
+let numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// place all possible lower case letters in an array & assign to a variable
 let lowerAlphabet = [
   "a",
   "b",
@@ -30,6 +34,7 @@ let lowerAlphabet = [
   "z",
 ];
 
+// place all possible upper case letters in an array & assign to a variable
 let upperAlphabet = [
   "A",
   "B",
@@ -59,46 +64,37 @@ let upperAlphabet = [
   "Z",
 ];
 
+// place all possible special characters in an array & assign to a variable
 let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
+// begin the builtArray empty & update value as the code runs
 let builtArray = [];
-
-// let builtArray = [...upperAlphabet, ...numbers];
-// let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
-
-// let builtArray = [...builtArray, ...numbers];
-
-// start at empty let builtArray = [] - > empty array
-// do you want numbers? let builtArray = [...builtArray, ...numbers] -> empty builtArray + numbers
-// do you want numbers and lowercase letters? [...builtArray, ...lowerAlphabet] -> builtArray gained numbers previously and now has lower case alphabet joined
-// do you want numbers and lowercase and uppercase? [...builtArray, upperAlphabet] -> builtArray gained lowercase & numbers previously and is now joined with upper case alphabet
-
-// lowerAlphabetExample[generatedLetterIndex];
 function generatePassword() {
-  let finalPassword = [];
-
+  // if confirmed, builtArray now contains all letters in lowerAlphabet. If not, then they are not incldued
   if (confirm("Do you want your password to contain lowercase letters?")) {
     builtArray = [...builtArray, ...lowerAlphabet];
-    // console.log(builtArray);
   }
+
+  // if confirmed, builtArray now contains all letters in upperAlphabet
   if (confirm("Do you want your password to contain uppercase letters?")) {
     builtArray = [...builtArray, ...upperAlphabet];
-    // console.log(builtArray);
   }
+  // if confirmed, builtArray now contains all numbers in numbersArray
   if (confirm("Do you want your password to contain numbers?")) {
-    builtArray = [...builtArray, ...numbers];
-    // console.log(builtArray);
+    builtArray = [...builtArray, ...numbersArray];
   }
+  // if confirm, builtArray now contains all special characters in specialCharacters
   if (confirm("Do you want your password to contain special characters?")) {
     builtArray = [...builtArray, ...specialCharacters];
-    // console.log(builtArray);
   }
-  console.log(builtArray);
 
+  // start with empty array to store received values
   let generatedLetterIndex = [];
-  let passwordLength = prompt("How long do you want the password to be?");
-  let password = "";
 
+  // prompt input decides the length of the password
+  let passwordLength = prompt("How long do you want the password to be?");
+
+  // min password length is 8, max is 128
   if (passwordLength < 8 || passwordLength > 128) {
     alert(
       "Password must be at least 8 characters but no more than 128 characters"
@@ -106,13 +102,16 @@ function generatePassword() {
     return;
   }
 
+  // randomly choose values from the builtArray
+  // continue x times based on passwordLength value
+  // push values into the generatedLetterIndex array
   for (let i = 0; i < passwordLength; i++) {
     generatedLetterIndex.push(
       builtArray[Math.floor(Math.random() * builtArray.length)]
     );
   }
 
-  console.log(generatedLetterIndex);
+  // return contents of the array as a string
   return generatedLetterIndex.join("");
 }
 
